e681215fb841ffdca2b923c4cc91767fcf51785b
Bryson Best <bryson.best@gmail.com>
2021-11-24 16:12:39 -0800

    Create function to submit a new url for shortening. Generate random string for shortURL.

--- express_server.js
+++ express_server.js
@@ -1,16 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080; //default port
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const generateRandomString = function() {
+  let randSt = "";
+  let characterBase = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for(i = 0; i < 6; i++) {
+    randSt += characterBase[((Math.round(Math.random() * (characterBase.length - 1))))];
+  }
+  return randSt;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n")
 });
 
 app.get("/urls.json", (req, res) => {
@@ -22,14 +37,19 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get('/urls/:shortURL', (req, res) => {
 //currently outputs as a text link in long URL
-  const templateVars = { shortURL: req.params.shortURL, longURL: `${urlDatabase[req.params.shortURL]}`}//* What goes here? */ };
+  const templateVars = { shortURL: req.params.shortURL, longURL: `${urlDatabase[req.params.shortURL]}`}
   res.render("urls_show", templateVars);;
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n")
+app.post("/urls", (req, res) => {
+  console.log(req.body);;
+  res.send("Ok");
 });
 
 app.listen(PORT, () => {
