7cf18c7f40be7e8ec3027df3458bbc2b8101aaba
Bryson Best <bryson.best@gmail.com>
2021-11-26 15:38:46 -0800

    Cookie and username using login/logout function.

--- express_server.js
+++ express_server.js
@@ -80,8 +80,11 @@ app.post("/urls/:shortURL/", (req, res) => {
 app.post("/login", (req, res) => {
   const username = req.body.username;
   res.cookie('username', username);
-  //console.log(req.body);
-  //res.cookie()
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
   res.redirect("/urls");
 });
 
